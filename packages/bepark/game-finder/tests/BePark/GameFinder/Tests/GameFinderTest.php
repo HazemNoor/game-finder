6280decf48799a64c8de5e28155d231624c400de
Hazem Noor <hazemnoor@gmail.com>
2021-03-29 17:54:52 +0200

    Remove not used test

--- packages/bepark/game-finder/tests/BePark/GameFinder/Tests/GameFinderTest.php
+++ packages/bepark/game-finder/tests/BePark/GameFinder/Tests/GameFinderTest.php
@@ -2,11 +2,8 @@
 
 namespace BePark\GameFinder\Tests;
 
-use BePark\GameFinder\Clients\AbstractClient;
-use BePark\GameFinder\Clients\IgdbClient;
+use BePark\GameFinder\Clients\ClientDecorator;
 use BePark\GameFinder\Clients\RawgClient;
-use BePark\GameFinder\GameFinder;
-use BePark\GameFinder\GameResult;
 use Exception;
 use Illuminate\Support\Collection;
 use Tests\TestCase;
@@ -18,85 +15,21 @@ class GameFinderTest extends TestCase
      */
     public function testClientRuntimeExceptionSuppress()
     {
-        $client = $this->getMockForAbstractClass(AbstractClient::class);
-
+        $client = $this->getMockBuilder(RawgClient::class)
+            ->disableOriginalConstructor()
+            ->getMock()
+        ;
         $client
             ->expects($this->once())
-            ->method('doSearch')
-            ->withAnyParameters()
+            ->method('search')
+            ->with('A Game Name', 10)
             ->willThrowException(new Exception())
         ;
 
-        $results = $client->search('A Game Name', 10);
+        $clientDecorator = new ClientDecorator($client);
+
+        $results = $clientDecorator->search('A Game Name', 10);
 
         $this->assertEquals(new Collection(), $results);
-    }
-
-    /**
-     * Test if different clients returned results have duplicate games data, duplicates will be removed
-     */
-    public function testRemoveDuplicateResults()
-    {
-        $collectionA = new Collection(
-            [
-                new GameResult('Game 1', 'https://example1.com/game1.jpg'),
-                new GameResult('Game 2', 'https://example1.com/game2.jpg'),
-                new GameResult('Game 3', 'https://example1.com/game3.jpg'),
-                new GameResult('Game 4', 'https://example1.com/game4.jpg'),
-                new GameResult('Game 5', 'https://example1.com/game5.jpg'),
-            ]
-        );
-        $collectionB = new Collection(
-            [
-                new GameResult('Game 4', 'https://example2.com/game4.jpg'),
-                new GameResult('Game 5', 'https://example2.com/game5.jpg'),
-                new GameResult('Game 6', 'https://example2.com/game6.jpg'),
-                new GameResult('Game 7', 'https://example2.com/game7.jpg'),
-                new GameResult('Game 8', 'https://example2.com/game8.jpg'),
-            ]
-        );
-
-        $expectedCollection = new Collection(
-            [
-                new GameResult('Game 1', 'https://example1.com/game1.jpg'),
-                new GameResult('Game 2', 'https://example1.com/game2.jpg'),
-                new GameResult('Game 3', 'https://example1.com/game3.jpg'),
-                new GameResult('Game 4', 'https://example1.com/game4.jpg'),
-                new GameResult('Game 5', 'https://example1.com/game5.jpg'),
-                new GameResult('Game 6', 'https://example2.com/game6.jpg'),
-                new GameResult('Game 7', 'https://example2.com/game7.jpg'),
-                new GameResult('Game 8', 'https://example2.com/game8.jpg'),
-            ]
-        );
-
-        $clientA = $this->getMockBuilder(RawgClient::class)
-            ->disableOriginalConstructor()
-            ->getMock()
-        ;
-        $clientA
-            ->expects($this->once())
-            ->method('search')
-            ->withAnyParameters()
-            ->willReturn($collectionA)
-        ;
-
-        $clientB = $this->getMockBuilder(IgdbClient::class)
-            ->disableOriginalConstructor()
-            ->getMock()
-        ;
-        $clientB
-            ->expects($this->once())
-            ->method('search')
-            ->withAnyParameters()
-            ->willReturn($collectionB)
-        ;
-
-        $finder = new GameFinder();
-        $finder->addClient($clientA);
-        $finder->addClient($clientB);
-
-        $results = $finder->search('A Game Name', false);
-
-        $this->assertEquals($expectedCollection->all(), $results);
     }
 }
